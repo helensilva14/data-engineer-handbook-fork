f4da7989a38f126fb0fa1d9e30063a5b97e9f306
Helen Cristina <helensilva14@gmail.com>
2025-09-11 17:56:29 -0400

    [MOD3] Initial version for Spark jobs for testing homework

---
+++ intermediate-bootcamp/materials/3-spark-fundamentals/notebooks/src/jobs/actors_scd_job.py
@@ -0,0 +1,58 @@
+from pyspark.sql import SparkSession
+
+query = """
+WITH change_indicators AS (
+    SELECT
+        actor_id, 
+        actor_name, 
+        quality_class, 
+        is_active,
+        current_year,
+        LAG(quality_class, 1) OVER (PARTITION BY actor_id ORDER BY current_year) 
+            <> quality_class AS quality_class_changed,
+        LAG(is_active, 1) OVER (PARTITION BY actor_id ORDER BY current_year) 
+            <> is_active AS is_active_changed
+    FROM actors
+),
+streak_identified AS (
+    SELECT
+        actor_id, 
+        actor_name, 
+        quality_class, 
+        is_active,
+        current_year,
+        SUM(CASE 
+                WHEN quality_class_changed THEN 1 
+                WHEN is_active_changed THEN 1 
+                ELSE 0
+            END) OVER (PARTITION BY actor_id ORDER BY current_year) 
+            AS streak_identifier
+    FROM change_indicators
+)
+-- INSERT INTO actors_history_scd
+SELECT
+    actor_id, 
+    actor_name, 
+    quality_class, 
+    is_active,
+    MIN(current_year) AS start_date,
+    MAX(current_year) AS end_date
+FROM streak_identified
+GROUP BY actor_id, actor_name, quality_class, is_active, streak_identifier
+ORDER BY actor_id, actor_name, start_date
+"""
+
+
+def do_actors_scd_transformation(spark, dataframe):
+    dataframe.createOrReplaceTempView("actors")
+    return spark.sql(query)
+
+
+def main():
+    spark = SparkSession.builder \
+      .master("local") \
+      .appName("actors_scd") \
+      .getOrCreate()
+    output_df = do_actors_scd_transformation(spark, spark.table("actors"))
+    output_df.write.mode("overwrite").insertInto("actors_scd")
+
---
+++ intermediate-bootcamp/materials/3-spark-fundamentals/notebooks/src/jobs/device_activity_datelist_job.py
@@ -0,0 +1,58 @@
+from pyspark.sql import SparkSession
+
+query = """
+WITH yesterday AS (
+    SELECT *
+    FROM user_devices_cumulated
+    WHERE record_date = DATE('2023-01-30')
+),
+events_ranked AS (
+    SELECT *, 
+        CAST(event_time AS DATE) AS event_date,
+        ROW_NUMBER() OVER 
+            (PARTITION BY user_id, device_id, CAST(event_time AS DATE) ORDER BY event_time) 
+            AS row_num
+    FROM events
+    WHERE user_id IS NOT NULL 
+),
+today AS (
+    SELECT
+        e.user_id,
+        e.event_date AS date_active,
+        d.browser_type
+    FROM events_ranked e
+    JOIN devices d ON e.device_id = d.device_id
+    WHERE e.event_date = DATE('2023-01-31') AND e.row_num = 1
+    GROUP BY e.user_id, e.event_date, d.browser_type
+)
+SELECT
+    COALESCE(t.user_id, y.user_id) AS user_id,
+    COALESCE(t.browser_type, y.browser_type) AS browser_type,
+    CASE 
+        WHEN y.device_activity_datelist IS NULL
+            THEN ARRAY[t.date_active]
+        WHEN t.date_active IS NULL
+            THEN y.device_activity_datelist
+        ELSE 
+            ARRAY[t.date_active] || y.device_activity_datelist
+    END
+        AS device_activity_datelist,
+    COALESCE(t.date_active, y.record_date + INTERVAL '1 DAY') AS record_date
+FROM today t
+FULL OUTER JOIN yesterday y
+ON y.user_id = t.user_id AND y.browser_type = t.browser_type
+"""
+
+
+def do_team_vertex_transformation(spark, dataframe):
+    dataframe.createOrReplaceTempView("device_activity")
+    return spark.sql(query)
+
+
+def main():
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("device_activity_datelist") \
+        .getOrCreate()
+    output_df = do_team_vertex_transformation(spark, spark.table("device_activity"))
+    output_df.write.mode("overwrite").insertInto("device_activity_datelist")
