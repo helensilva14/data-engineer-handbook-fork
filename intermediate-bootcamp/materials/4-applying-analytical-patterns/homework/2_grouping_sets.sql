0b9c9cdc297a711c869052534f8d0226838aa2bb
Helen Cristina <helensilva14@gmail.com>
2025-09-17 16:24:56 -0400

    [MOD4] Refactor grouping sets query

--- intermediate-bootcamp/materials/4-applying-analytical-patterns/homework/2_grouping_sets.sql
+++ intermediate-bootcamp/materials/4-applying-analytical-patterns/homework/2_grouping_sets.sql
@@ -1,3 +1,4 @@
+CREATE TABLE game_details_dashboard AS
 WITH game_details_augmented AS (
     SELECT
         gd.game_id AS game_id,
@@ -7,32 +8,52 @@ WITH game_details_augmented AS (
         COALESCE(gd.pts, 0) AS pts,
         CASE
             WHEN g.home_team_id = gd.team_id AND g.home_team_wins = 1
-                THEN True
-            ELSE False
+                THEN TRUE
+            WHEN g.visitor_team_id = gd.team_id AND g.home_team_wins = 0
+                THEN FALSE
+            ELSE FALSE
         END AS team_won
     FROM game_details gd
     JOIN games g ON gd.game_id = g.game_id
-    WHERE gd.start_position IS NOT NULL
 )
 SELECT
     CASE
-        WHEN GROUPING(player_name) = 0
-            AND GROUPING(team) = 0
-                THEN 'player_name__team'
-        WHEN GROUPING(player_name) = 0
-            AND GROUPING(season) = 0
-                THEN 'player_name__season'
+        WHEN GROUPING(player_name) = 0 AND GROUPING(team) = 0
+            THEN 'player_name__team'
+        WHEN GROUPING(player_name) = 0 AND GROUPING(season) = 0
+            THEN 'player_name__season'
         WHEN GROUPING(team) = 0 THEN 'team'
     END AS aggregation_level,
-    COALESCE(player_name, '(overall)') as player_name,
-    COALESCE(team, '(overall)') as team,
-    COALESCE(CAST(season AS TEXT), '(overall)') as season,
-    SUM(pts) as total_points,
-    SUM(CASE WHEN team_won IS TRUE THEN 1 ELSE 0 END) as total_wins,
-    COUNT(*) as total_games
+    COALESCE(player_name, '(overall)') AS player_name,
+    COALESCE(team, '(overall)') AS team,
+    COALESCE(CAST(season AS TEXT), '(overall)') AS season,
+    SUM(pts) AS total_points,
+    COUNT(DISTINCT game_id) FILTER (WHERE team_won IS TRUE) AS total_wins,
+    COUNT(DISTINCT game_id) AS total_games
 FROM game_details_augmented
 GROUP BY GROUPING SETS (
     (player_name, team),
     (player_name, season),
     (team)
-)
+);
+
+-- Question 1: Who scored the most points playing for one team?
+SELECT *
+FROM game_details_dashboard
+WHERE aggregation_level = 'player_name__team'
+ORDER BY total_points DESC
+LIMIT 1;
+
+-- Question 2: Who scored the most points in one season?
+SELECT *
+FROM game_details_dashboard
+WHERE aggregation_level = 'player_name__season'
+ORDER BY total_points DESC
+LIMIT 1;
+
+-- Question 3: Which team has won the most games?
+SELECT *
+FROM game_details_dashboard
+WHERE aggregation_level = 'team'
+ORDER BY total_wins DESC
+LIMIT 1;
